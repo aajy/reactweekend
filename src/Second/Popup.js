8715d2d682439f1ab71dd6c9552ecba971a4ccfc
aajy <aajy.000@gmail.com>
2023-07-07 14:22:44 +0900

    버튼에 따라 배경이미지 바꾸는 확인 팝업 만들기 작업

--- src/Second/Popup.js
+++ src/Second/Popup.js
@@ -1,8 +1,12 @@
-function Popup() {
+function Popup({color}) {
   return (
-    <div className='popup'>
-      팝업입니다.
-    </div>
+    <>
+      <div className='popup' style={{backgroundColor : color}}>
+        해당 컬러로 선택 하시 겠습니까?
+        <button>OK</button>
+        <button>Choose again</button>
+      </div>
+    </>
   )
 }
 
--- src/Second/Second.js
+++ src/Second/Second.js
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import '../scss/secondStyle.scss'
 import Popup from './Popup';
+import img from '../img/black.jpg'
 
 function Second() {
-  const isPopup = true;
+  const [isPopup, setIsPopup] = useState(false);
+  const [color, setColor] = useState('black');
+  const buttons =['Red', 'Orange', 'Green', 'Blue', 'Pink' ];
+  const changeColor = (e) => {
+    console.log(e.target.innerText)
+    console.log(isPopup)
+    setIsPopup(true);
+    setColor(e.target.innerText);
+  };
   return (
-    <div>
-      {isPopup && <Popup/> }
-    </div>
+    <>
+      { buttons.map((button, idx) => {
+        return (
+        <button key={idx} onClick={changeColor}>
+          {button}
+        </button>
+        )
+      })}
+      {isPopup && <Popup color={color}/> }
+      <img className="img" src={img} alt="img"/>
+    </>
   )
 }
 
